3158ede4a1221c2d4a6f7ee2c0cc284146a52c6b
Kennedy Pinheiro <kpx@aluno.ifnmg.edu.br>
2024-05-21 21:54:41 -0300

    Update media.js

--- media.js
+++ media.js
@@ -10,24 +10,29 @@ abaixo da função o console.log imprime o valor da media das notas.
 
 */
 
-function medianotas(valor,valor1,valor2){
+function medianotas() {
+    let aluno = prompt("Me informe o nome do aluno: ");
     let quantidadeNotas = parseInt(prompt("Quantas notas você quer inserir?"));
     let notas = [];
 
     for (let i = 0; i < quantidadeNotas; i++) {
-        let nota =(prompt(`Insira a nota ${i + 1}:`));
+        let nota = parseFloat(prompt(`Insira a nota ${i + 1}:`));
         notas.push(nota);
     }
 
-   let soma = 0;
+    let soma = 0;
     for (let i = 0; i < notas.length; i++) {
         soma += notas[i];
     }
 
     let media = soma / quantidadeNotas;
-    return media;
 
-    }
+    return { aluno, media };
+}
 
-let media = medianotas();
-console.log(`A média das notas é: ${media}`)
+let resultado = medianotas();
+let aluno = resultado.aluno;
+let media = resultado.media;
+let aprovacao = media >= 7 ? "Aprovado" : "Reprovado";
+
+console.log(`O Aluno ${aluno} teve a média ${media}, ${aprovacao}`);
